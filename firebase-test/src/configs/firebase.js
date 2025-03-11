ec432c80ce05835accf57eca62e76535f07e132f
Sarahtlenglish <sarah_english@live.dk>
2025-03-11 09:55:39 +0100

    Init project

---
+++ firebase-test/src/configs/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjXPS992dVwQS5QcOwZOhyAyg1j8fYJ9E",
+  authDomain: "mfd25-bf838.firebaseapp.com",
+  projectId: "mfd25-bf838",
+  storageBucket: "mfd25-bf838.firebasestorage.app",
+  messagingSenderId: "624324460247",
+  appId: "1:624324460247:web:fe849a450d8825959d3a92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
